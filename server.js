343396067ae2499edadc43c0880d58f669211e3e
lindsayjohnston <lkjohnston10@gmail.com>
2022-12-29 14:25:18 -0700

    Was able to send something to the websocket and console.log it

--- server.js
+++ server.js
@@ -14,11 +14,40 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on('message', function message(data){
+    console.log('received: %s', data)
+  })
   ws.on('close', () => console.log('Client disconnected'));
 });
+
+
+
+
 
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
+
+// const express = require('express');
+// const http = require('http');
+// const WebSocket = require('ws');
+
+// const port = 3000;
+// const server = http.createServer(express);
+// const wss = new WebSocket.Server({ server })
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(data);
+//       }
+//     })
+//   })
+// })
+
+// server.listen(port, function() {
+//   console.log(`Server is listening on ${port}!`)
+// })
