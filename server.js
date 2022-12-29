1fbc416af56a4e4b0588c0c2f31568e5c1239085
lindsayjohnston <lkjohnston10@gmail.com>
2022-12-29 15:56:06 -0700

    Added in some basic chat styling.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+const messages = [] //[{ from: <username>, message: <message>}]
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
